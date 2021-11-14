aa524735fd68c31ed5999b7921e9739b855cfb35
Vallie Tracy <trac0055@umn.edu>
2021-11-14 10:09:55 -0600

    have final list though very much a hack job

--- data/pdf_2.py
+++ data/pdf_2.py
@@ -25,6 +25,7 @@ all_pages_list = parse_pdf()
 
 def remove_n(arr):
     """Remove '\n from a list'"""
+    
     target = '\n'
     cleaned_list = []
     
@@ -45,6 +46,7 @@ def foreign_chars(arr):
 
     ok = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz -/'
     funky_words = []
+    
     for word in arr:
         for letter in word:
             if letter not in ok:
@@ -66,59 +68,230 @@ def remove_funky(arr):
     return funks_removed
 
 cleaner_list = remove_funky(funky_words)
+print(len(cleaner_list))
 
-spaces = []
-no_spaces = []
-for word in cleaner_list:
+def edge_case_empty(arr):
+    """I discovered that there are five cases of elements that are empty but not picked up
+    in other functions, so I'm removing them in this function quick.  5 elements total."""
+    
+    cleaner_list = []
+    for word in arr:
+        if word != '':
+            cleaner_list.append(word)
+    return cleaner_list
+
+cleaner_list = edge_case_empty(cleaner_list)
+print(len(cleaner_list))
+
+def create_space_lists(arr):
+    """Loop thru a list and determine if an element starts with a space.
+    Depending on that, it will then add it to one of two newly created lists.
+    Will return a set of two lists."""
+
+    spaces = []
+    no_spaces = []
+
+    for word in arr:
+        if word.startswith(' '):
+            spaces.append(word)
+        else:
+            no_spaces.append(word)
+
+    return (spaces, no_spaces)
+
+both_lists = create_space_lists(cleaner_list)
+spaces_list = both_lists[0]
+no_spaces_list = both_lists[1]
+
+# Do I need this function? Not quite sure, but it does create some useful data
+def find_spaces(arr, space_count):
+    """Loop thru a list, count number of spaces in each item and return a dictionary
+    with element as key and count as value.
+    Based on number of spaces specified (parameter space_count), will also return keys + number of keys.
+    Space_count is an integer. Returns those three items as a set"""
+    
+    target = ' '
+    dict_of_words = {}
+    
+    for word in arr:
+        counter = 0
+        for letter in word:
+            if letter == target:
+                counter += 1
+        dict_of_words[word] = counter
+
+    my_keys = []
+    for key, value in dict_of_words.items():
+        if value >= space_count:
+            my_keys.append(key)
+    return (my_keys, len(my_keys), dict_of_words)
+
+# ************************************************
+# no_spaces_list is good, no spaces at front, no multiple spaces between, have not checked end yet
+# check for spaces at end once I have all words into one list again
+# for i in range(len(no_spaces_list)):
+#     print(f'{i}.{no_spaces_list[i]}')
+# ************************************************
+
+# Broken up spaces_list into two lists to make it easier to see in terminal
+spaces_list_1 = []
+for i in range(0, 1000):
+    spaces_list_1.append(spaces_list[i])
+spaces_list_2 = []
+for i in range(1000, len(spaces_list)):
+    spaces_list_2.append(spaces_list[i])
+
+new_spaces_1 = []
+for word in spaces_list_1:
+    word = word[1:]
+    new_spaces_1.append(word)
+bad_words_1 = []
+for word in new_spaces_1:
     if word.startswith(' '):
-        spaces.append(word)
+        bad_words_1.append(word)
+#print(bad_words_1)
+good_bad_words_1 = []
+for word in bad_words_1:
+    word = word[1:]
+    good_bad_words_1.append(word)
+#print(good_bad_words_1)
+# good_bad_words_1 is good list from spaces_list_1
+spaces_1_minus_bad = []
+for word in new_spaces_1:
+    if word not in bad_words_1:
+        spaces_1_minus_bad.append(word)
+
+#print(len(spaces_1_minus_bad))
+largest_1 = []
+for word in spaces_1_minus_bad:
+    if word != '':
+        largest_1.append(word)
+#print(len(largest_1))
+
+new_spaces_2 = []
+for word in spaces_list_2:
+    word = word[1:]
+    new_spaces_2.append(word)
+bad_words_2 = []
+for word in new_spaces_2:
+    if word.startswith(' '):
+        bad_words_2.append(word)
+print(bad_words_2)
+good_bad_words_2 = []
+for word in bad_words_2:
+    word = word[1:]
+    good_bad_words_2.append(word)
+print(good_bad_words_2)
+# good_bad_words_2 is good list from spaces_list_2
+spaces_2_minus_bad = []
+for word in new_spaces_2:
+    if word not in bad_words_2:
+        spaces_2_minus_bad.append(word)
+
+print(len(spaces_2_minus_bad))
+largest_2 = []
+for word in spaces_2_minus_bad:
+    if word != '':
+        largest_2.append(word)
+
+# largest_2 has two instances where there are two items separated by two spaces
+print(f'length of largest_2: {len(largest_2)}')
+new_words = []
+for item in largest_2:
+    if '  ' in item:
+        words = item.split('  ')
+        for word in words:
+            new_words.append(word)
+print(new_words)
+next_new_words = []
+for word in new_words:
+    if word.startswith(' '):
+        word = word[1:]
+        next_new_words.append(word)
     else:
-        no_spaces.append(word)
+        next_new_words.append(word)
+print(next_new_words)
+for item in largest_2:
+    if '  ' in item:
+        largest_2.remove(item)
+print(f'length of largest_2: {len(largest_2)}')
+print(len(next_new_words))
+print(len(largest_2))
+
+
+
+        
+
+
+# spaces_list_1 and spaces_list_2 were created from spaces_list
+# spaces_list has a length of around 1982 I believe
+# I brought it into two parts
+# spaces_list_1 was broken down further, and those parts are done
+# those two parts that are done are called largest_1 and good_bad_words_1
+# to keep all lists together, no_spaces_list is also good
+# PUT LISTS in BELOW TO CHECK THEM LINE BY LINE IN TERMINAL:
+# for i in range(len(largest_2)):
+#     print(f'{i}.{largest_2[i]}')
+
+
+# no_spaces_list is good
+# largest_1 is good
+# good_bad_words_1 is good
+# good_bad_words_2 is good
+# largest_2 is good
+# next_new_words is good
+
+# NEED TO JOIN THE ABOVE 6 LISTS!
+
+lists = (no_spaces_list, largest_1, largest_2, good_bad_words_1, good_bad_words_2, next_new_words)
+final_list = no_spaces_list+largest_1+largest_2+good_bad_words_1+good_bad_words_2+next_new_words
+print(f'the length of final list is {len(final_list)}')
+endings = []
+for word in final_list:
+    if word[-1] == ' ':
+        final_list.remove(word)
+        word = word[:-1]
+        endings.append(word)
+        
+
+
+result = final_list + endings
+# DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
+
+
+
+
+
  
-counter = 0
-for word in no_spaces:
-    if len(word) == 0:
-        counter += 1
-print(counter)
-
-# KEEP THIS CHUNK OF CODE BELOW THIS LINE!
-new_no_spaces = []
-for word in no_spaces:
-    if len(word) != 0:
-        new_no_spaces.append(word)
-print(f'new_no_spaces length is {len(new_no_spaces)}')
-# This is good what's right above this line!!!!!!!
-
-counter = 0
-for word in spaces:
-    if len(word) == 0:
-        counter += 1
-print(counter)
- 
- # VALLIE, Do any words end with a space? Something to keep in mind perhaps...
-
-print(spaces)
 
 
 
 
 
-ok = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz '
-target = ' '
 
-# dict_of_words = {}
-# for word in cleaner_list:
+
+# print(len(new_spaces))
+
+# still_spaces = []
+# for word in new_spaces:
 #     counter = 0
-#     for letter in word:
-#         if letter == target:
-#             counter += 1
-#     dict_of_words[word] = counter
+#     if word.startswith(' '):
+#         still_spaces.append(word)
 
-# my_keys = []
-# for key, value in dict_of_words.items():
-#     if value >= 2:
-#         my_keys.append(key)
-# print(my_keys)
-# print(len(my_keys))
+# new_still_spaces = []
+# for word in new_still_spaces:
+#     word = word[1:]
+#     new_still_spaces.append(word)
+
+
+
+
+
+
+
+
 
     
